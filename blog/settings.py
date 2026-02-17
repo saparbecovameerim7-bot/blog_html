1035195b3564c5f7d156aec4815c4396b2542f27
Ваше Имя <.email@example.com>
2026-02-17 17:21:13 +0600

    update settings

--- blog/settings.py
+++ blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q=ne++zq(jda46=35u+r$#l@unc31%p*2htf)^-e1j(*@7o$^$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'blog-app.onrender.com']
 
 
 # Application definition
